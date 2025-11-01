9ad313935a3a2cfccbcf4507f391773451d4f38a
Thomas Naskali <thomas@naska.li>
2025-11-01 23:27:44 +0200

    fix script on wsl

--- ansible/install.sh
+++ ansible/install.sh
@@ -13,12 +13,8 @@ abort() {
 }
 
 reload_shell_profile() {
-  if [ -f "$HOME/.bashrc" ]; then
-    . "$HOME/.bashrc"
-  elif [ -f "$HOME/.bash_profile" ]; then
+  if [ -f "$HOME/.bash_profile" ]; then
     . "$HOME/.bash_profile"
-  elif [ -f "$HOME/.zshrc" ]; then
-    . "$HOME/.zshrc"
   elif [ -f "$HOME/.zprofile" ]; then
     . "$HOME/.zprofile"
   elif [ -f "$HOME/.profile" ]; then
@@ -43,11 +39,6 @@ echo "📦 Installing or upgrading pipx using pip..."
 python3 -m pip install --break-system-packages --user pipx
 # Adds pipx local binary directory to PATH
 python3 -m pipx ensurepath --force
-cat $HOME/.bashrc || true
-cat $HOME/.bash_profile || true
-cat $HOME/.profile || true
-echo $SHELL
-echo $PATH
 
 echo "📦 Installing ansible using pipx..."
 python3 -m pipx install --include-deps --force ansible
